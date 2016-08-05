073f21327c483e7318295239de7f4ae005a9194e
sq-ios92 <sq-ios92@sq-ios92demacbook-air.local>
2016-08-05 10:10:55 +0800

    Initial Commit

---
+++ plement/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  plement
+//
+//  Created by sq-ios92 on 16/8/5.
+//  Copyright © 2016年 xxt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ plement/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  plement
+//
+//  Created by sq-ios92 on 16/8/5.
+//  Copyright © 2016年 xxt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
